cbffb3edfb6594c71efcfd0f0ef66218bd89a502
Kun Huang <Kun.Huang@bnymellon.com>
2018-03-25 09:35:25 -0400

    1)Finished  RxJs Part 1

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -16,7 +16,7 @@ export class AboutComponent implements OnInit {
   constructor(private leaderService : LeaderService) { }
   ngOnInit() {
     this.leaderService.getLeaders()
-      .then(leaders =>  this.leaders = leaders);
+      .subscribe(leaders =>  this.leaders = leaders);
   }
 
 }
