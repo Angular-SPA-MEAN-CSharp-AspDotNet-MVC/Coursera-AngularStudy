5dfe539b6d8ad432ddbf173f36123ca825481264
Kun Huang <Kun.Huang@bnymellon.com>
2018-03-30 12:05:42 -0400

    1) Finished  REST (Restangular)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import { FormsModule} from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RestangularModule, Restangular } from 'ngx-restangular';
+import {RestangularConfigFactory} from './shared/restConfig';
 
 import 'hammerjs';
 
@@ -50,7 +52,8 @@ import { ProcessHTTPMsgService } from './services/process-httpmsg.service';
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
-    HttpModule
+    HttpModule,
+    RestangularModule.forRoot(RestangularConfigFactory)
   ],
   providers: [ DishService, PromotionService, LeaderService,
     { provide: 'BaseURL', useValue: baseURL },
--- src/app/services/dish.service.ts
+++ src/app/services/dish.service.ts
@@ -1,43 +1,36 @@
 import { Injectable } from '@angular/core';
 import { Dish } from '../shared/dish';
-// import { DISHES } from '../shared/dishes';
 import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 
 import { baseURL } from '../shared/baseurl';
 import { ProcessHTTPMsgService } from './process-httpmsg.service';
+import { RestangularConfigFactory } from '../shared/restConfig';
 
 import 'rxjs/add/operator/delay';
 import 'rxjs/add/observable/of';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
+import {Restangular} from "ngx-restangular";
 
 @Injectable()
 export class DishService {
 
-  constructor(private http: Http,
+  constructor(private restangular: Restangular,
               private processHTTPMsgService: ProcessHTTPMsgService) {}
 
   getDishes(): Observable<Dish[]> {
-    // return Observable.of(DISHES).delay(2000);
-    return this.http.get(baseURL + 'dishes')
-      .map(res => { return this.processHTTPMsgService.extractData(res);      })
-      .catch( error => {return this.processHTTPMsgService.handleError(error); });
+    return this.restangular.all('dishes').getList();
   }
 
   getDish(id: number): Observable<Dish> {
-    // return Observable.of(DISHES.filter( (dish) => (dish.id === id) )[0]).delay(2000);
-    return this.http.get(baseURL + 'dishes/' + id)
-      .map( res => { return this.processHTTPMsgService.extractData(res) })
-      .catch( error => { return this.processHTTPMsgService.handleError(error); });
+    return this.restangular.one('dishes', id).get();
   }
 
   getFeaturedDish(): Observable<Dish> {
-    // return Observable.of(DISHES.filter( (dish) => (dish.featured) )[0]).delay(2000);
-    return this.http.get(baseURL + 'dishees?feature=true' )
-      .map( res => { return this.processHTTPMsgService.extractData(res)[0]; })
-      .catch( error => { return this.processHTTPMsgService.handleError(error); });
+    return this.restangular.all('dishes').getList({feature: true})
+      .map( dishes => dishes[0] );
   }
 
   getDishIds(): Observable<number[]> {
