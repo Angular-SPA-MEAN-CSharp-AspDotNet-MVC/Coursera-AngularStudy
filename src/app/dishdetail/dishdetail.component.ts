f26d0724be2d3a4c0298f5b5f9a612c2aaf1a556
Kun Huang <Kun.Huang@bnymellon.com>
2018-03-31 11:24:11 -0400

    1) Finished  Animations Part 1

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, Inject } from '@angular/core';
 import { Params, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { trigger, state, style, animate, transition } from '@angular/animations';
+
 
 import { Dish } from '../shared/dish';
 import { DishService } from '../services/dish.service';
@@ -9,11 +11,26 @@ import { Comment } from '../shared/comment';
 
 import 'rxjs/add/operator/switchMap';
 
+
 @Component({
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
-  styleUrls: ['./dishdetail.component.scss']
+  styleUrls: ['./dishdetail.component.scss'],
+  animations: [
+    trigger('visibility', [
+      state('shown', style({
+        transform: 'scale(1.0)',
+        opacity: 1
+      })),
+      state('hidden', style({
+        transform: 'scale(0.5)',
+        opacity: 0
+      })),
+      transition('* => *', animate('0.5s ease-in-out'))
+    ])
+  ]
 })
+
 export class DishdetailComponent implements OnInit {
 
   dish: Dish;
@@ -24,6 +41,7 @@ export class DishdetailComponent implements OnInit {
   addCommentForm: FormGroup;
   commentForm: Comment;
   errMess: string;
+  visibility = 'shown';
 
   commentErrors = {
     'rating':   '',
@@ -87,9 +105,9 @@ export class DishdetailComponent implements OnInit {
       .subscribe(dishIds => this.dishIds = dishIds);
 
     this.route.params
-      .switchMap((params: Params) => this.dishservice.getDish(+params['id']))
-      .subscribe( dish => {this.dish = dish; this.setPrevNext(dish.id);} ,
-        errmess => this.errMess = <any>errmess );
+      .switchMap((params: Params) => { this.visibility = 'hidden'; return this.dishservice.getDish(+params['id']); })
+      .subscribe( dish => { this.dish = dish; this.dishcopy = dish; this.setPrevNext(dish.id); this.visibility = 'shown'; } ,
+        errmess => { this.dish = null; this.errMess = <any>errmess; });
   }
 
   setPrevNext(dishId: number){
