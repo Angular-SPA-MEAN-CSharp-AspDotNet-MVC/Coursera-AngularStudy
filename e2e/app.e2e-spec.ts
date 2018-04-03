0ab63b47dfad17bee2e10ca294f0cd7f2dbd6fc3
Kun Huang <Kun.Huang@bnymellon.com>
2018-04-03 16:58:27 -0400

    1)Coded the End-to-End Test. (Not working, need to debug.)

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,4 +1,5 @@
 import { AppPage } from './app.po';
+import { browser } from 'protractor';
 
 describe('con-fusion App', () => {
   let page: AppPage;
@@ -7,8 +8,35 @@ describe('con-fusion App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('should display message saying Ristorante Con Fusion', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getParagraphText('app-root h1')).toEqual('Ristorante Con Fusion');
   });
+
+  it('should navigate to about us page by clicking on the link', () => {
+   page.navigateTo('/');
+
+   let navlink = page.getALLElements('a').get(1); // this is the 'about' link
+    navlink.click();
+
+    expect(page.getParagraphText('h3')).toBe('About Us');
+  });
+
+  it( 'should enter a new comment for the first dish', () => {
+    page.navigateTo('/dishdetail/0');
+
+    let newAuthor = page.getElement('input[type=text]');
+    newAuthor.sendKeys('Test Author');
+
+    let newComment = page.getElement('textarea');
+    newComment.sendKeys('Test Comment');
+
+    let newSubmittButton = page.getElement('button[type=submit]');
+    new newSubmittButton.click();
+
+    browser.pause();
+  }
+
+
+  )
 });
